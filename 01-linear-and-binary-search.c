9c086710f7f637abeb77a400397b54c9eb593dc0
Naman Arora <namanarora1022@gmail.com>
2024-08-12 09:25:43 +0530

    refactor: linear and binary search

--- 01-linear-and-binary-search.c
+++ 01-linear-and-binary-search.c
@@ -4,6 +4,18 @@
 void inputArray(int arr[], int n);
 void printArray(int arr[], int n);
 
+// TC: O(N)
+// SC: O(1)
+int linear_search(int arr[], int n, int target)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+            return i;
+    }
+    return -1;
+}
+
 // TC: O(log(N)), here log is with base 2
 // SC: O(1)
 int binary_search(int arr[], int n, int target)
@@ -37,7 +49,8 @@ int main()
     printf("Enter target: ");
     scanf("%d", &target);
 
-    printf("Element found at index: %d\n", binary_search(arr, n, target));
+    printf("Element found at index(linear): %d\n", linear_search(arr, n, target));
+    printf("Element found at index(binary): %d\n", binary_search(arr, n, target));
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # 📘 Data Structures Lab Codes
 
-1. WAP for Linear search
-2. WAP for Binary search
+1. WAP for Linear search and Binary search
